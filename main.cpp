8ea176da342595d6d7ad92b8ba5aaf801fcdb144
Preet2204 <preetsiddhapura2204@gmail.com>
2025-06-02 09:25:00 +0530

    Print All Methods

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 #include <cmath>
 #include <functional>
+#include <iomanip>
 using namespace std;
 
-float Trapezoidal(float lower_limit, float upper_limit, int intervals, function<float(float)> func) {
+float Trapezoidal(float lower_limit, float upper_limit, int intervals, function<float(float)>   func) {
     
     float h = (upper_limit - lower_limit) / intervals;
     
@@ -53,17 +54,21 @@ float Simpson38(float lower_limit, float upper_limit, int intervals, function<fl
         sum += 2 * func(lower_limit + i * h);
     }
     
-    float integ = 3 * h * sum / 8;
+    float integral = 3 * h * sum / 8;
     
-    return integ;
+    return integral;
 }
 
 int main() {
     
     auto func1 = [](float x) -> float { return x*x + 3*x + 2; };
+    auto func1integral = [](float x) -> float { return (x*x*x/3 + 3*x*x/2 + 2*x); };
     auto func2 = [](float x) -> float { return sin(5 * x) / 2 + 1; };
+    auto func2integral = [](float x) -> float { return (-cos(5 * x) / 10 + x); };
     auto func3 = [](float x) -> float { return 3 * cos(2 * x) + 1; };
+    auto func3integral = [](float x) -> float { return (3 * sin(2 * x) / 2 + x); };
     auto func4 = [](float x) -> float { return 2 * exp(3 * x); };
+    auto func4integral = [](float x) -> float { return 2 * exp(3 * x) / 3; };
     
     cout << "\nWelcome to the Numerical Integrator!\n\n";
     while(true) {
@@ -88,39 +93,25 @@ int main() {
         }
         
         function<float(float)> func;
+        function<float(float)> funcintegral;
         
         switch(funcChoice) {
             case 1:
                 func = func1;
+                funcintegral = func1integral;
                 break;
             case 2:
                 func = func2;
+                funcintegral = func2integral;
                 break;
             case 3:
                 func = func3;
+                funcintegral = func3integral;
                 break;
             case 4:
                 func = func4;
+                funcintegral = func4integral;
                 break;
-        }
-
-        cout << "\nChoose the method you like to use to integrate the function. (Enter number)" << '\n';
-        cout << "1. Trapezoidal Rule" << '\n';
-        cout << "2. Simpson's 1/3 Rule" << '\n';
-        cout << "3. Simpson's 3/8 Rule" << '\n';
-        cout << "4. Exit" << "\n";
-
-        int choice;
-        cin >> choice;
-
-        while(choice < 1 || choice > 5) {
-            cout << "Invalid choice.\n";
-            cin >> choice;
-        }
-
-        if(choice == 4) {
-            cout << "Exiting...\n";
-            return 0;
         }
 
         cout << "\nEnter the lower limit of integration: ";
@@ -133,13 +124,16 @@ int main() {
         
         while(lower_limit > upper_limit) {
             cout << "Lower Limit can't be greater than Upper Limit. (Try Again)\n\n";
-            cout << "Enter the lower limit of integration: ";
+            cout << "Enter the lower limit of Integration: ";
             cin >> lower_limit;
             cout << "Enter the upper limit of Integration: ";
             cin >> upper_limit;
         }
-
-        cout << "Enter the number of intervals: ";
+        
+        cout << '\n';
+        cout << "For Simpson's 1/3 rule intervals Should be multiple of 2.\n";
+        cout << "For Simpson's 3/8 rule intervals Should be multiple of 3.\n";
+        cout << "Enter the number of intervals ";
         int intervals;
         cin >> intervals;
         while(intervals <= 0) {
@@ -147,35 +141,54 @@ int main() {
             cin >> intervals;
         }
         
-        float integ;
-        switch (choice) {
-            case 1:
-                integ = Trapezoidal(lower_limit, upper_limit, intervals, func);
-                cout << "\nIntegration (Trapezoidal) = " << integ;
-                break;
-            case 2:
-                while(intervals % 2 != 0) {
-                    cout << "For Simpson's 1/3 Rule intervals should be divisible by 2. Enter Intervals Again: ";
-                    cin >> intervals;
-                }
-                integ = Simpson13(lower_limit, upper_limit, intervals, func);
-                cout << "\nIntegration (Simpson's 1/3) = " << integ;
-                break;
-            case 3:
-                while(intervals % 3 != 0) {
-                    cout << "For Simpson's 3/8 Rule intervals should be divisible by 3. Enter Intervals Again: ";
-                    cin >> intervals;
-                }
-                integ = Simpson38(lower_limit, upper_limit, intervals, func);
-                cout << "\nIntegration (Simpson's 3/8) = " << integ;
-                break;
-            case 4:
-                cout << "Exiting...\n";
-                return 0;
-            default:
-                cout << "Invalid choice";
-                break;
+        cout << '\n';
+        int col1 = 15, col2 = 25, col3 = 25, col4 = 25;
+        float integral[3];
+        float actualIntegral = funcintegral(upper_limit) - funcintegral(lower_limit);
+        float error[3];
+        
+        // Printing Header
+        cout << left << setw(col1) << " " << setw(col2) << "Trapezoidal" << setw(col3) << "Simpson's 1/3" << setw(col4) << "Simpson's 3/8" << '\n';
+        
+        // Printinf 1st Row
+        integral[0] = Trapezoidal(lower_limit, upper_limit, intervals, func);
+        error[0] = abs(integral[0] - actualIntegral);
+        cout << left << setw(col1) << "Integral";
+        cout << left << setw(col2) << fixed << setprecision(6) << integral[0];
+        
+        if(intervals % 2 == 0) {
+            integral[1] = Simpson13(lower_limit, upper_limit, intervals, func);
+            error[1] = abs(integral[1] - actualIntegral);
+            cout << left << setw(col3) << fixed << setprecision(6) << integral[1];
+        }else {
+            cout << left << setw(col3) << "Invalid Intervals!";
         }
+        
+        if(intervals % 3 == 0) {
+            integral[2] = Simpson38(lower_limit, upper_limit, intervals, func);
+            error[2] = abs(integral[2] - actualIntegral);
+            cout << left << setw(col4) << fixed << setprecision(6) << integral[2];
+        }else {
+            cout << left << setw(col4) << "Invalid Intervals!";
+        }
+        
+        cout << '\n';
+        // Printing 2nd Row
+        cout << left << setw(col1) << "Error";
+        cout << left << setw(col2) << fixed << setprecision(12) << error[0];
+        
+        if(intervals % 2 == 0) {
+            cout << left << setw(col3) << fixed << setprecision(12) << error[1];
+        }else {
+            cout << left << setw(col3) << "Invalid Intervals!";
+        }
+        
+        if(intervals % 3 == 0) {
+            cout << left << setw(col4) << fixed << setprecision(12) << error[2];
+        }else {
+            cout << left << setw(col4) << "Invalid Intervals!";
+        }
+        
         cout << "\n\n";
     }
     
